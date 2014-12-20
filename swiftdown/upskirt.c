bbb660db6492b4e9faecd9f5085c31cef89603f3
Jakob Gürtl <jakob.guertl@me.com>
2014-12-20 15:03:19 +0100

    upskirt files

---
+++ swiftdown/upskirt.c
@@ -0,0 +1,43 @@
+#include "markdown.h"
+#include "html.h"
+#include "buffer.h"
+
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define READ_UNIT 1024
+#define OUTPUT_UNIT 64
+
+
+
+struct buf* markdownify (char *in) {
+    struct buf *ib, *ob, *ob1;
+    struct mkd_renderer renderer;
+    
+    /* reading everything */
+    ib = bufnew (READ_UNIT);
+    ib->size = strlen (in);
+    ib->data = in;
+    
+    ob = bufnew (OUTPUT_UNIT);
+    ob->size = 0;
+    
+    ob1 = bufnew (OUTPUT_UNIT);
+    ob1->size = 0;
+    
+    
+    /* Markdown parse */
+    upshtml_renderer (&renderer, 65535);
+    ups_markdown (ob, ib, &renderer, 65535);
+    upshtml_free_renderer (&renderer);
+    bufrelease (ib);
+    
+    upshtml_smartypants (ob1, ob);
+    bufrelease (ob);
+    
+    
+    return ob1;
+}
